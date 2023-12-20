c590509ce13d800fcb7a7ee60935ddf99701c97b
pradyumna <pradyumnakaranth18@gmail.com>
2023-12-20 12:13:05 +0530

    first

---
+++ src/main/java/com/example/Dailyquestion/DailyquestionApplication.java
@@ -0,0 +1,13 @@
+package com.example.Dailyquestion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyquestionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DailyquestionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Dailyquestion/DailyquestionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Dailyquestion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailyquestionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
